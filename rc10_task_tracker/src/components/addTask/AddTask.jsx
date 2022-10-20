37583fcef2c593be80553f7007d430582cd05c9c
berkay oren <benberkayoren@gmail.com>
2022-10-20 21:55:38 +0300

    a

--- rc10_task_tracker/src/components/addTask/AddTask.jsx
+++ rc10_task_tracker/src/components/addTask/AddTask.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+import Form from "react-bootstrap/Form";
 
 const AddTask = () => {
-  return (
-    <div>AddTask</div>
-  )
-}
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
 
-export default AddTask
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { task, date };
+    console.log("task geldi mi:", task);
+    console.log(date);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Task</Form.Label>
+          <Form.Control
+            type="Text"
+            placeholder="Enter Task"
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Date</Form.Label>
+          <Form.Control
+            type="date"
+            onChange={(e) => {
+              e.target.value;
+            }}
+          />
+        </Form.Group>
+
+        <Button variant="primary  w-50" type="submit">
+          SAVE
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddTask;
