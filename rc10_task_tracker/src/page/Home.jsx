46e9492419ec30a7bbe668248e0dc2842cf82c21
berkay oren <benberkayoren@gmail.com>
2022-10-20 20:57:58 +0300

    commit

--- rc10_task_tracker/src/page/Home.jsx
+++ rc10_task_tracker/src/page/Home.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+// import React, { useState } from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
+import Button from "react-bootstrap/Button";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState("Show Task Bar");
+  const url = "https://6351827ddfe45bbd55c22952.mockapi.io/tasktrack/tasks";
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    const buttonText = isOpen ? "Show Task Bar" : "Hide Task Bar";
+    setText(buttonText);
+  };
+
+  //CRUD - READ
+  const getTask = async () => {
+    const { data } = await axios(url);
+    setTask(data);
+  };
+
+  useEffect(() => {
+    getTask();
+  }, []);
+
   return (
     <div>
-      Home
+      <Button
+        onClick={(e) => {
+          toggle();
+        }}
+        variant="danger"
+        size="lg"
+      >
+        {text}
+      </Button>
+      {isOpen && <AddTask />}
       <AddTask />
       <TaskList />
     </div>
